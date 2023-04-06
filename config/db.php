9fe96a1d792ff3d360c634aca5e2041cf7efc45e
mikzanco <mikyzancolo97@gmial.com>
2023-04-06 10:25:16 +0200

    creato db e TebleSeeder

---
+++ config/db.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+
+    "dishes" => [
+        [
+            "titolo" => "Alici del Cantabrico con Crostini e Burro",
+            "tipo" => "Antipasto",
+            "src" => "https://www.jamonarium.com/3003-home_default/acciuga-del-cantabrico-in-olio-di-oliva-yurrita-50g.jpg",
+            "descrizione" => "Letto di acciughe del Cantabrico servite con riccioli di burro e crostini di pane. Le Acciughe del Cantabrico sono note per la qualità della materia prima e per il processo produttivo artigianale, di gran lunga superiore alle altre, ma, pur lavorando da diversi anni su un sigillo di qualità che la certifica, un'Indicazione Geografica che tutela questo buon lavoro.",
+            "allegeni" => "1, 4, 7",
+            "prezzo" => "11",
+        ],
+        [
+            "titolo" => "Porcini fritti",
+            "tipo" => "Antipasto",
+            "src" => "https://tempodicottura.it/wp-content/uploads/2020/08/IMG_0404.jpg",
+            "descrizione" => "Insalata di porcini fritti accompagnati da scaglie di grana.",
+            "allegeni" => "1, 4, 7",
+            "prezzo" => "13",
+        ],
+        [
+            "titolo" => "Carpaccio di cervo",
+            "tipo" => "Antipasto",
+            "src" => "https://www.firstonline.info/wp-content/uploads/2022/11/unnamed-480x270.jpg",
+            "descrizione" => "CArpaccio di cervo servito con una panna cotta ai profumi di Pino Mugo",
+            "allegeni" => "1, 7",
+            "prezzo" => "13",
+        ],
+        [
+            "titolo" => "Tomino caldo",
+            "tipo" => "Antipasto",
+            "src" => "https://blog.giallozafferano.it/mammaluisa/wp-content/uploads/2015/09/100_3148b.jpg",
+            "descrizione" => "Letto di Cappucci e Mele che accolgono un Tomino servito caldo con Miele e Noci",
+            "allegeni" => "1, 7, 8",
+            "prezzo" => "11",
+        ]
+    ]
+]
